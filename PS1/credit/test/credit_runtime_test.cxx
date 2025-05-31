b8af2385b2eb2276cb5fc1d3bac9d3f1b7ea9cfc
Bran Tregare <rss@tregare.net>
2025-05-31 16:22:04 +0200

    Update from upstream private repo

--- PS1/credit/test/credit_runtime_test.cxx
+++ PS1/credit/test/credit_runtime_test.cxx
@@ -6,31 +6,31 @@ using credit::test::test_validate;
 
 TEST_CASE("Valid card numbers pass Luhn and match card type", "[valid]")
 {
-  SECTION("VISA 16-digit") { REQUIRE(test_validate(4111111111111111) == card_type_t::VISA); }
+  SECTION("VISA 16-digit") { REQUIRE(test_validate(4111111111111111) == CardType::VISA); }
 
-  SECTION("AMEX") { REQUIRE(test_validate(378282246310005) == card_type_t::AMEX); }
+  SECTION("AMEX") { REQUIRE(test_validate(378282246310005) == CardType::AMEX); }
 
-  SECTION("MASTERCARD") { REQUIRE(test_validate(5105105105105100) == card_type_t::MASTERCARD); }
+  SECTION("MASTERCARD") { REQUIRE(test_validate(5105105105105100) == CardType::MASTERCARD); }
 
-  SECTION("VISA 13-digit") { REQUIRE(test_validate(4222222222222) == card_type_t::VISA); }
+  SECTION("VISA 13-digit") { REQUIRE(test_validate(4222222222222) == CardType::VISA); }
 }
 
 TEST_CASE("Invalid cards fail due to checksum", "[invalid][luhn]")
 {
-  REQUIRE(test_validate(4111111111111112) == card_type_t::INVALID);
-  REQUIRE(test_validate(378282246310004) == card_type_t::INVALID);
+  REQUIRE(test_validate(4111111111111112) == CardType::INVALID);
+  REQUIRE(test_validate(378282246310004) == CardType::INVALID);
 }
 
 TEST_CASE("Invalid cards fail due to length or unsupported prefix", "[invalid][prefix]")
 {
-  REQUIRE(test_validate(6011111111111117) == card_type_t::INVALID); // Discover not supported
-  REQUIRE(test_validate(123) == card_type_t::INVALID);
-  REQUIRE(test_validate(0) == card_type_t::INVALID);
+  REQUIRE(test_validate(6011111111111117) == CardType::INVALID); // Discover not supported
+  REQUIRE(test_validate(123) == CardType::INVALID);
+  REQUIRE(test_validate(0) == CardType::INVALID);
   // NOTE: This test is commented out because the input exceeds the internal digit buffer size.
   // With -Werror and constexpr validation, this is caught at compile time.
   // Uncommenting this test may cause compilation to fail.
   // NOTE: This test is commented out because the input exceeds the internal digit buffer size.
   // With -Werror and constexpr validation, this is caught at compile time.
   // Uncommenting this test may cause compilation to fail.
-  // REQUIRE(test_validate(41111111111111112222ULL) == card_type_t::INVALID); // Too long
+  // REQUIRE(test_validate(41111111111111112222ULL) == CardType::INVALID); // Too long
 }
