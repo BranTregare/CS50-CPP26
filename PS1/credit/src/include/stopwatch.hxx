818a66e920abcd3ea987360dc0b73b7bcf611f17
Bran Tregare <rss@tregare.net>
2025-06-16 17:54:08 +0200

    Update from private CS50 repo

--- PS1/credit/src/include/stopwatch.hxx
+++ PS1/credit/src/include/stopwatch.hxx
@@ -15,33 +15,33 @@ class StopWatch
   using Clock = std::chrono::high_resolution_clock;
 
 private:
-  std::chrono::time_point<Clock> last;
-  std::string name_;
+  std::chrono::time_point<Clock> Last_;
+  std::string Name_;
 
 public:
   inline void reset() noexcept
   {
-    last = Clock::now();
+    Last_ = Clock::now();
   }
   inline auto operator()() const noexcept
   {  // returns time in Resolution
-    auto duration = std::chrono::duration<double, std::nano>(Clock::now() - last);
-    return duration;
+    auto Duration = std::chrono::duration<double, std::nano>(Clock::now() - Last_);
+    return Duration;
   }
-  StopWatch(const std::string name = "unamed") noexcept : name_(name)
+  StopWatch(const std::string Name = "unamed") noexcept : Name_(Name)
   {
-    std::print("SW {}:\n", name_);
+    std::print("SW {}:\n", Name_);
     reset();
   }
   ~StopWatch()
   {
     // bad encapulation, you should reconsider this design!
     // e.g. take a std::function as argument to the constructor and call it here
-    auto endtime = (*this)();
+    auto End_Time = (*this)();
     // std::println("SW ~ {0}: ns  :{1:20} ", name_, std::chrono::duration<double, std::nano>(endtime));
-    std::println("SW ~ {0}: µs  :{1:10} ", name_, std::chrono::duration<double, std::micro>(endtime));
-    std::println("SW ~ {0}: ms  :{1:10} ", name_, std::chrono::duration<double, std::milli>(endtime));
-    std::println("SW ~ {0}: s   :{1:10} ", name_, std::chrono::duration<double>(endtime));
+    std::println("SW ~ {0}: µs  :{1:10} ", Name_, std::chrono::duration<double, std::micro>(End_Time));
+    std::println("SW ~ {0}: ms  :{1:10} ", Name_, std::chrono::duration<double, std::milli>(End_Time));
+    std::println("SW ~ {0}: s   :{1:10} ", Name_, std::chrono::duration<double>(End_Time));
   }
 };
 #endif  // STOPWATCH_HXX
